d3cdf07f0714ab262ec329a04a8c4c5926987f12
Нейверт Арина <a.neyvert@gmail.com>
2025-09-26 01:03:56 +0700

    Условия в шаблоне

--- app.py
+++ app.py
@@ -369,8 +369,4 @@ def example():
     lab_number = '2'    
     group = 'ФБИ-33'
     course = '3'
-    return render_template('example.html', 
-                          name=name, 
-                          lab_number=lab_number, 
-                          group=group, 
-                          course=course)
+    return render_template('example.html')
