7f0eea8d59466d77a965bcaf9be276c184985c91
Нейверт Арина <a.neyvert@gmail.com>
2025-09-25 21:25:10 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -366,4 +366,11 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Арина Нейверт'
-    return render_template('example.html', name=name)
+    lab_number = '2'    
+    group = 'ФБИ-33'
+    course = '3'
+    return render_template('example.html', 
+                          name=name, 
+                          lab_number=lab_number, 
+                          group=group, 
+                          course=course)
