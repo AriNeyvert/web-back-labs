95e1c85c2e55459107dd77e89864739f2652d68e
Нейверт Арина <a.neyvert@gmail.com>
2025-11-12 15:26:20 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
+import psycopg2
 
 app = Flask(__name__)
 
@@ -16,10 +16,6 @@ def index():
 def login():
     return "Страница входа"
 
-@lab5.route('/lab5/register')
-def register():
-    return "Страница регистрации"
-
 @lab5.route('/lab5/list')
 def list_articles():
     return "Список статей"
@@ -27,3 +23,34 @@ def list_articles():
 @lab5.route('/lab5/create')
 def create_article():
     return "Создать статью"
+
+@lab5.route('/lab5/register', methods=['GET', 'POST'])
+def register_user():  # Изменил имя функции, чтобы избежать конфликта
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/register.html', error='Заполните все поля')
+    
+    conn=psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'arina_neyvert_knowledge_base',
+        user = 'arina_neyvert_knowledge_base',
+        password = '1967'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template('lab5/register.html',
+                               error="Такой пользователь уже существует")
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html', login=login)
