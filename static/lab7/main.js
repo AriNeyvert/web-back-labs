040b3e7ddd1a4bd4376ba06e731577f378810fdd
Нейверт Арина <a.neyvert@gmail.com>
2025-12-05 17:53:47 +0700

    Добавление фильма

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -1,3 +1,63 @@
+// Глобальные функции для работы с модальным окном
+function showModal() {
+    document.querySelector('div.modal').style.display = "block";
+}
+
+function hideModal() {
+    document.querySelector('div.modal').style.display = 'none';
+}
+
+function cancel() {
+    hideModal();
+}
+
+function addFilm() {
+    document.getElementById('id').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('title-ru').value = '';
+    document.getElementById('year').value = '';
+    document.getElementById('description').value = '';
+    showModal();
+}
+
+function sendFilm() {
+    const film = {
+        title: document.getElementById('title').value,
+        title_ru: document.getElementById('title-ru').value,
+        year: document.getElementById('year').value,
+        description: document.getElementById('description').value
+    };
+
+    const url = `/lab7/rest-api/films/`;
+    const method = 'POST';
+    fetch(url, {
+        method: method,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(film)
+    })
+    .then(function() {
+        fillFilmList();
+        hideModal();
+    })
+    .catch(function(error) {
+        console.error('Ошибка при добавлении фильма:', error);
+    });
+}
+
+function deleteFilm(id, title) {
+    if(!confirm(`Вы точно хотите удалить фильм "${title}"?`)) {
+        return;
+    }
+
+    fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
+    .then(function() {
+        fillFilmList();
+    })
+    .catch(function(error) {
+        console.error('Ошибка при удалении фильма:', error);
+    });
+}
+
 function fillFilmList() {
     fetch('/lab7/rest-api/films/')
     .then(function(data){
@@ -23,20 +83,11 @@ function fillFilmList() {
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
-            delButton.onclick = function() {
-                deleteFilm(i, films[i].title_ru);
-            };
-            function deleteFilm(id, title) {
-                if(! confirm(`Вы точно хотите удалить фильм "${title}"?`))
-                    return;
-
-                fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
-                .then(function () {
-                    fillFilmList();
-                    
-                });
-
-            }
+            delButton.onclick = (function(id, title) {
+                return function() {
+                    deleteFilm(id, title);
+                };
+            })(films[i].id || i, films[i].title_ru);
 
             tdActions.append(editButton);
             tdActions.append(delButton);
