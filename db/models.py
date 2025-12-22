3530d844038f9e9500c341c4b1106514432ece22
Нейверт Арина <a.neyvert@gmail.com>
2025-12-22 22:02:21 +0700

    с 4 з

--- db/models.py
+++ db/models.py
@@ -1,21 +1,15 @@
 from . import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    
-    articles = db.relationship('Article', backref='author', lazy=True)
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    login = db.Column(db.String(30), nullable = False, unique = True)
+    password = db.Column(db.String(162), nullable = False)
 
-class Article(db.Model):
-    __tablename__ = 'articles'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    article_text = db.Column(db.Text, nullable=False)
-    is_public = db.Column(db.Boolean, default=False)
-    likes = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    is_favorite = db.Column(db.Boolean, default=False)
+class articles(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    login_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(50), nullable = False)
+    article_text = db.Column(db.Text, nullable = False)
+    is_favorite = db.Column(db.Boolean)
+    is_public = db.Column(db.Boolean)
+    likes = db.Column(db.Integer)
