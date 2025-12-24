e0fc18efddc6a0d95b83b8a76dc45ab6eeb6bff4
Нейверт Арина <a.neyvert@gmail.com>
2025-12-24 21:46:52 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz_bp
 import datetime
 import os
@@ -54,6 +55,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz_bp)
 
 # Глобальная переменная для хранения лога 404 ошибок
@@ -161,6 +163,7 @@ def index():
                 <li><a href="/lab6">Шестая лабораторная</a></li>
                 <li><a href="/lab7">Седьмая лабораторная</a></li>
                 <li><a href="/lab8">Восьмая лабораторная</a></li>
+                <li><a href="/lab9">Девятая лабораторная</a></li>
                 <li><a href="/rgz" class="rgz-link">Расчетно-графическое задание: Кинотеатр</a></li>
             </ul>
             <footer>
